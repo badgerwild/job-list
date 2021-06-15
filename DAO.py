330c242b47313d08f5cf025bcf274d65069ccf7b
badgerwild <jklimock@gmail.com>
2021-06-15 17:25:23 -0400

    Add update methods for application table, add select method to fins jobs based on status

--- DAO.py
+++ DAO.py
@@ -2,6 +2,11 @@ import sqlite3
 
 
 class DAO:
+    def connect(self):
+        con = sqlite3.connect('Jobs.db')
+        cur = con.cursor()
+        return con, cur
+
     def add_single_job(self, company, title, level, url, date):
         con = sqlite3.connect('Jobs.db')
         cur = con.cursor()
@@ -30,7 +35,7 @@ class DAO:
         return values
 
     def apply(self, job_id, status, date):
-        con = sqlite3.connect('Application')
+        con = sqlite3.connect('Jobs.db')
         cur = con.cursor()
         application = (job_id, status, date)
         cur.execute(
@@ -39,8 +44,73 @@ class DAO:
         con.commit()
         con.close()
 
+    def update_date(self, job_id, date):
+        con, cur = self.connect()
+        data = (date, job_id)
+        cur.execute(
+            "UPDATE Application SET response_date=? WHERE job_id=?", data
+        )
+        con.commit()
+        con.close()
+
+    def update_status(self, job_id, status):
+        con, cur = self.connect()
+        data = (job_id, status)
+        cur.execute(
+            "UPDATE Application SET status=? WHERE job_id=?", data
+        )
+        con.commit()
+        con.close()
+
+    def update_response_date(self, job_id, date):
+        con, cur = self.connect()
+        data = (job_id, date)
+        cur.execute(
+            "UPDATE Application SET response_date=? WHERE job_id=?", data
+        )
+        con.commit()
+        con.close()
+
+    def update_number_of_interviews(self, job_id, number):
+        con, cur = self.connect()
+        data = (job_id, number)
+        cur.execute(
+            "UPDATE Application SET number_of_interviews=? WHERE job_id=?", data
+        )
+        con.commit()
+        con.close()
+
+    def leet_code(self, job_id, leet):
+        con, cur = self.connect()
+        data = (job_id, leet)
+        cur.execute(
+            "UPDATE Application SET leet_code=? WHERE job_id=?", data
+        )
+        con.commit()
+        con.close()
+
+    def code_challenge(self, job_id, value):
+        con, cur = self.connect()
+        data = (job_id, value)
+        cur.execute(
+            "UPDATE Application SET leet_code=? WHERE job_id=?", data
+        )
+        con.commit()
+        con.close()
+
     def status(self):
         con = sqlite3.connect('Jobs.db')
         cur = con.cursor()
         cur.execute('SELECT * FROM status')
         return cur.fetchall()
+
+    # TODO test on the DB
+    def find_status(self, status):
+        con, cur = self.connect()
+        cur.execute(
+            "SELECT * FROM Jobs WHERE job_id IN (SELECT job_id FROM Application WHERE status=?)", status
+        )
+        data = cur.fetchall()
+        values = self.make_dict(data)
+        return values
+
--- main.py
+++ main.py
@@ -1,44 +1,84 @@
 import datetime
 from DAO import DAO
 
-db = DAO()
 
-exit = False
-validae = db.status()
+if __name__ == '__main__':
+    db = DAO()
+    exit = False
+    validate = db.status()[0]
 
-while not exit:
-    print('Welcome to job search app')
-    print('Add single job: 1')
-    print('Find jobs by companay: 2')
-    print('Apply to Job: 3')
+    while not exit:
+        print('Welcome to job search app')
+        print('Add single job: 1')
+        print('Find jobs by companay: 2')
+        print('Apply to Job: 3')
+        print("Update application date: 4")
+        print('Update application status: 5 ')
+        print('Update response date: 6')
+        print('Update number of interviews: 7 ')
+        print('update if leet code: 8')
+        print('Update if code challenge: 9')
 
-    choice = input('enter choice: ')
-#TODO refactor to abstract logic from main view
-    if int(choice) == 1:
-        print('Yes!!')
-        company_name = input('enter company name: ')
-        job_title = input('enter job title: ')
-        job_level = input('enter job level: ')
-        url = input('enter url: ')
-        date = input('enter date poasted (yyyy-mm-dd: ')
-        db.add_single_job(company_name, job_title, job_level, url, date)
 
-    elif int(choice) == 2:
-        company_name = input('enter company name: ')
-        print(db.find_company(company_name))
+        choice = input('enter choice: ')
+    # TODO refactor to abstract logic from main view
+        if int(choice) == 1:
+            print('Yes!!')
+            company_name = input('enter company name: ')
+            job_title = input('enter job title: ')
+            job_level = input('enter job level: ')
+            url = input('enter url: ')
+            date = input('enter date poasted (yyyy-mm-dd: ')
+            db.add_single_job(company_name, job_title, job_level, url, date)
 
-    elif int(choice) == 3:
-        company_name = input('enter company name: ')
-        print(db.find_company(company_name))
-        apply_to = input('enter job id from list: ')
-        status = ''
-        while status not in validae:
-            print(validae)
-            status = input('enter a valid status: ')
-        date = datetime.date
-        db.apply(apply_to, status, date)
+        elif int(choice) == 2:
+            company_name = input('enter company name: ')
+            print(db.find_company(company_name))
 
-    finished = input('are you finished? (y,n): ')
+        elif int(choice) == 3:
+            company_name = input('enter company name: ')
+            print(db.find_company(company_name))
+            apply_to = input('enter job id from list: ')
+            status = ''
+            while status not in validate:
+                print(validate)
+                status = input('enter a valid status: ')
+            date = str(datetime.date.today())
+            db.apply(apply_to, status, date)
 
-    if finished == 'y':
-        exit = True
+        elif int(choice) == 4:
+            date = input('enter date applied (yyyy-mm-dd): ')
+            job_id = input('enter job id: ')
+            db.update_date(job_id, str(date))
+
+        elif int(choice) == 5:
+            job_id = input('enter job id: ')
+            status = input('Enter application status')
+            while status not in validate:
+                print(validate)
+                status = input('enter a valid status: ')
+            db.update_status(job_id, status)
+
+        elif int(choice) == 6:
+            job_id = input('enter job id: ')
+            response_date = input("Enter response date")
+            db.update_response_date(job_id, response_date)
+
+        elif int(choice) == 7:
+            job_id = input('enter job id: ')
+            interviews = input('enter number of interviews: ')
+            db.update_number_of_interviews(job_id, interviews)
+
+        elif int(choice) == 8:
+            job_id = input('enter job id:')
+            db.leet_code(job_id, 1)
+
+        elif int(choice) == 9:
+            job_id = input('enter job id')
+            db.code_challenge(job_id, 1)
+
+
+        finished = input('are you finished? (y,n): ')
+
+        if finished == 'y':
+            exit = True
